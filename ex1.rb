f1d3b9ef48aa82adf9cdc8b0e49b453eff993dbd
Tom Snowden <tom.snowden1@gmail.com>
2020-10-22 09:14:05 +0000

    rb100arrays

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+
+my_pets = pets[2]
+print my_pets
+=begin
+Select 'fish' from pets, 
+assign the return value to a variable named my_pet, 
+then print the value of my_pet.
+=end
---
+++ ex6.rb
@@ -0,0 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+
+ puts numbers.map {|x| x *2}
+ 
+
